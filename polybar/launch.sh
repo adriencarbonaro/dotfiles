f8e2090f1150e1cd13ba4c37952decdfafcd1e91
Adrien CARBONARO <carbonaro.adrien@gmail.com>
2019-07-28 10:41:28 +0200

    I3/POLYBAR setup for multi-monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -13,12 +13,20 @@
 # Dotfiles :    www.github.com/adriencarbonaro/dotfiles.git
 #------------------------------------------------------------
 
+bar="carbo"
+
 # Terminate already runnning bars
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch carbo bar
-polybar carbo &
+# Set monitor(s)
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload $bar &
+    done
+else
+    polybar --reload $bar &
+fi
 
