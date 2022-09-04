39b28b0c10e0980d2a00824008232d9cb440915f
Adrien CARBONARO <carbonaro.adrien@gmail.com>
2022-09-04 11:03:07 +0200

    add polybar install script

---
+++ polybar/install.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+
+CONFIG_DIR=$HOME/.config
+
+PROGRAM="polybar"
+
+PROGRAM_DIR=$CONFIG_DIR/$PROGRAM
+PROGRAM_BACKUP_DIR=$CONFIG_DIR/$PROGRAM.back
+
+if [ -d $PROGRAM_DIR ]; then
+    echo "Backing up current $PROGRAM..."
+    if [ -d $PROGRAM_BACKUP_DIR ]; then
+        echo "Backup already exists. Exiting ..."
+        exit
+    else
+        mv $PROGRAM_DIR $PROGRAM_BACKUP_DIR
+    fi
+else
+    echo "No current config"
+fi
+
+echo "Copying $PROGRAM config..."
+cp -r . $PROGRAM_DIR
+
