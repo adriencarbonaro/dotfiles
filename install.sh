eb15892824b0929ee5ae7030a3c709ea46fd99a5
Adrien CARBONARO <carbonaro.adrien@gmail.com>
2023-01-09 18:28:02 +0100

    install: remove unnecessary, prepare for sub install scripts

--- install.sh
+++ install.sh
@@ -1,72 +1,12 @@
 #!/bin/sh
 
-TOPDIR=$HOME/dotfiles/
+TOPDIR=$(dirname ${BASH_SOURCE:-$0})
 
-######################
-# Directory creation #
-######################
-mkdir -p $HOME/.config/i3
-mkdir -p $HOME/.config/polybar
-mkdir -p $HOME/.config/rofi
-mkdir -p $HOME/.config/terminator
-mkdir -p $HOME/.vim/
-mkdir -p $HOME/bin
+FG_BLUE="\033[34m"
+FG_GREY="\033[37m"
 
-#######
-# ack #
-#######
-cp $TOPDIR/ack/.ackrc $HOME/
+echo "${FG_BLUE}Installing Adrien's dotfiles..."
+echo "${FG_GREY}  Dotfiles: ${TOPDIR}"
 
-###########
-# compton #
-###########
-cp $TOPDIR/compton/compton.conf $HOME/.config/
-
-#######
-# git #
-#######
-cp $TOPDIR/git/.gitconfig $HOME/
-
-######
-# i3 #
-######
-cp $TOPDIR/i3/config $HOME/.config/i3/config
-
-#############
-# oh-my-zsh #
-#############
-cp $TOPDIR/ohmyzsh/themes/* $HOME/.oh-my-zsh/themes
-
-###########
-# polybar #
-###########
-cp $TOPDIR/polybar/config $HOME/.config/polybar/config
-
-########
-# rofi #
-########
-cp $TOPDIR/rofi/carbo.rasi $HOME/.config/rofi/carbo.rasi
-cp $TOPDIR/rofi/config $HOME/.config/rofi/config
-
-##############
-# terminator #
-##############
-cp $TOPDIR/terminator/config $HOME/.config/terminator/config
-
-#########
-# URxvt #
-#########
-cp $TOPDIR/urxvt/.Xdefaults $HOME/.Xdefaults
-
-#######
-# vim #
-#######
-cp $TOPDIR/vim/.vimrc $HOME/.vimrc
-cp -r $TOPDIR/vim/.vim $HOME/
-
-#######
-# zsh #
-#######
-cp $TOPDIR/zsh/.zshrc $HOME/.zshrc
-cp $TOPDIR/zsh/.bash_aliases $HOME/.bash_aliases
+# Call each sub-install scripts
 
